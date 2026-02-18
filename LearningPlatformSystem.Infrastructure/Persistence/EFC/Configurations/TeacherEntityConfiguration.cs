f12e531d60e754799bcbf51bdb065df5d4c522d3
AlayaStHill <alaya.st.hill@gmail.com>
2026-02-18 14:02:37 +0100

    feat: add EF Core configuration for TeacherEntity

---
+++ LearningPlatformSystem.Infrastructure/Persistence/EFC/Configurations/TeacherEntityConfiguration.cs
@@ -0,0 +1,65 @@
+﻿using LearningPlatformSystem.Domain.Shared.ValueObjects.Addresses;
+using LearningPlatformSystem.Domain.Shared.ValueObjects.ContactInformations;
+using LearningPlatformSystem.Domain.Shared.ValueObjects.PersonNames;
+using LearningPlatformSystem.Infrastructure.Persistence.EFC.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LearningPlatformSystem.Infrastructure.Persistence.EFC.Configurations;
+
+public class TeacherEntityConfiguration : EntityBaseConfiguration<TeacherEntity>
+{
+    public override void Configure(EntityTypeBuilder<TeacherEntity> builder)
+    {
+        base.Configure(builder);
+               
+        builder.ToTable("Teachers");
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.Id)
+        .ValueGeneratedNever();
+
+        //PersonName = ownedType (ägs av en annan entitet och lagras i samma tabell)
+        builder.OwnsOne(teacher => teacher.Name, name =>
+        {
+            // Mappar value objectets egenskaper till kolumner i tabellen
+            name.Property(n => n.FirstName)
+                .HasColumnName("FirstName")
+                .HasMaxLength(PersonName.FirstNameMaxLength)
+                .IsRequired();
+
+            name.Property(n => n.LastName)
+                .HasColumnName("LastName")
+                .HasMaxLength(PersonName.LastNameMaxLength)
+                .IsRequired();
+        });
+
+        builder.OwnsOne(teacher => teacher.ContactInformation, contactInfo =>
+        {
+            contactInfo.Property(c => c.Email)
+                .HasColumnName("Email")
+                .HasMaxLength(ContactInformation.EmailMaxLength)
+                .IsRequired();
+            contactInfo.Property(c => c.PhoneNumber)
+                .HasColumnName("PhoneNumber")
+                .HasMaxLength(ContactInformation.PhoneNumberMaxLength)
+                .IsRequired();
+        });
+
+        builder.OwnsOne(teacher => teacher.Address, address =>
+        {
+            address.Property(a => a.Street)
+                .HasColumnName("Street")
+                .HasMaxLength(Address.StreetMaxLength);
+            address.Property(a => a.City)
+                .HasColumnName("City")
+                .HasMaxLength(Address.CityMaxLength);
+            address.Property(a => a.PostalCode)
+                .HasColumnName("PostalCode")
+                .HasMaxLength(Address.PostalCodeMaxLength);
+        });
+
+        //Index:
+        builder.HasIndex(entity => new { entity.Name.FirstName, entity.Name.LastName });
+    }
+}
--- LearningPlatformSystem.Infrastructure/Persistence/EFC/Entities/TeacherEntity.cs
+++ LearningPlatformSystem.Infrastructure/Persistence/EFC/Entities/TeacherEntity.cs
@@ -1,31 +1,13 @@
-﻿using LearningPlatformSystem.Domain.Shared.ValueObjects.PersonNames;
+﻿using LearningPlatformSystem.Domain.Shared.ValueObjects.Addresses;
+using LearningPlatformSystem.Domain.Shared.ValueObjects.ContactInformations;
+using LearningPlatformSystem.Domain.Shared.ValueObjects.PersonNames;
 namespace LearningPlatformSystem.Infrastructure.Persistence.EFC.Entities;
 
 public sealed class TeacherEntity : EntityBase
 {
     public Guid Id { get; set; }
     public PersonName Name { get; set; } = null!;
-    public Guid ContactInformationId { get; set; }
-    public ContactInformationEntity ContactInformation { get; set; } = null!;
+    public ContactInformation ContactInformation { get; set; } = null!;
+    public Address? Address { get; set; }
 }
 
-/*
- Konfigurera relationen till ContactInformation:
-builder.HasOne(t => t.ContactInformation)
-       .WithOne() // eller .WithOne(ci => ci.Teacher) om du har back-navigation
-       .HasForeignKey<TeacherEntity>(t => t.ContactInformationId);
-
-PersonName mappas som ownedTypy:
-builder.OwnsOne(t => t.Name, name =>
-{
-    name.Property(n => n.FirstName)
-        .HasColumnName("FirstName")
-        .HasMaxLength(50)
-        .IsRequired();
-
-    name.Property(n => n.LastName)
-        .HasColumnName("LastName")
-        .HasMaxLength(50)
-        .IsRequired();
-});
-*/
