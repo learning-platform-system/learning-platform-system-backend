575c636e176205647917aad5b0271da3246378e1
AlayaStHill <alaya.st.hill@gmail.com>
2026-02-18 12:42:39 +0100

    feat: Add EF Core persistence setup Install EF Core packages (SqlServer, Tools, Design)and introduce  LearningPlatformDbContext. Add shared EntityBaseConfiguration for auditing (CreatedAt, ModifiedAt) and concurrency (RowVersion).

--- LearningPlatformSystem.Infrastructure/Persistence/EFC/LearningPlatformDbContext.cs
+++ LearningPlatformSystem.Infrastructure/Persistence/EFC/LearningPlatformDbContext.cs
@@ -1,5 +1,28 @@
-﻿namespace LearningPlatformSystem.Infrastructure.Persistence.EFC;
+﻿using LearningPlatformSystem.Infrastructure.Persistence.EFC.Entities;
+using Microsoft.EntityFrameworkCore;
 
-public class LearningPlatformDbContext
+namespace LearningPlatformSystem.Infrastructure.Persistence.EFC;
+public class LearningPlatformDbContext : DbContext
 {
+    public LearningPlatformDbContext(DbContextOptions<LearningPlatformDbContext> options) : base(options)
+    {
+    }
+    // tabellen för ClassroomEntity hämtas från DbContext via Set<T>().
+    public DbSet<ClassroomEntity> Classrooms => Set<ClassroomEntity>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // EF Core letar efter ALLA klasser i assemblyn (projektet) som implementerar IEntityTypeConfiguration och anropar deras Configure-metod automatiskt.
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(LearningPlatformDbContext).Assembly);
+    }
 }
+
+/*
+När DbContext startar:
+→ EF bygger modellen
+→ EF anropar OnModelCreating
+→ EF hittar dina IEntityTypeConfiguration-klasser
+→ EF anropar Configure(builder)
+*/
