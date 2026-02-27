41ed625b3a5faf0e17688972c9da92798ea0f7d2
AlayaStHill <alaya.st.hill@gmail.com>
2026-02-27 16:58:07 +0100

    test: Add unit tests for Subcategory

---
+++ LearningPlatsformSystem.Domain.Tests/Aggregates/SubcategoryTests.cs
@@ -0,0 +1,40 @@
+﻿using LearningPlatformSystem.Domain.Shared.Exceptions;
+using LearningPlatformSystem.Domain.Subcategories;
+
+namespace LearningPlatformSystem.Domain.Tests.Aggregates;
+
+public class SubcategoryTests
+{
+    [Fact]
+    public void Create_ShouldThrowDomainException_WhenCategoryIdIsEmpty()
+    {
+        // Arrange (förbered)
+        Guid id = Guid.NewGuid();
+        Guid categoryId = Guid.Empty;
+        string name = "Backend";
+
+        // Act (agera/kör)
+        Action act = () => Subcategory.Create(id, categoryId, name);
+
+        // Assert (verifiera) - använder DINA errors
+        DomainException exception = Assert.Throws<DomainException>(act);
+        Assert.Equal(SubcategoryErrors.CategoryIdIsRequired, exception.Message);
+    }
+
+    [Fact]
+    public void Rehydrate_ShouldCreateInstance_WithGivenValues()
+    {
+        // Arrange (förbered)
+        Guid id = Guid.NewGuid();
+        Guid categoryId = Guid.NewGuid();
+        string name = "Databaser";
+
+        // Act (agera/kör)
+        Subcategory subcategory = Subcategory.Rehydrate(id, categoryId, name);
+
+        // Assert (verifiera)
+        Assert.Equal(id, subcategory.Id);
+        Assert.Equal(categoryId, subcategory.CategoryId);
+        Assert.Equal(name, subcategory.Name);
+    }
+}
