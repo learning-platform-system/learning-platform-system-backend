86df02753b6f8eeed9e22b383b5bed69bb572353
AlayaStHill <alaya.st.hill@gmail.com>
2026-02-28 11:52:58 +0100

    test: Add unit tests for TeacherService

---
+++ LearningPlatformSystem.Application.Tests/Teachers/TeacherServiceTestBase.cs
@@ -0,0 +1,27 @@
+﻿using LearningPlatformSystem.Application.Shared;
+using LearningPlatformSystem.Application.Teachers;
+using LearningPlatformSystem.Domain.Teachers;
+using Moq;
+
+namespace LearningPlatformSystem.Application.Tests.Teachers;
+
+public abstract class TeacherServiceTestBase
+{
+    protected readonly Mock<ITeacherRepository> TeacherRepositoryMock;
+    protected readonly Mock<IUnitOfWork> UnitOfWorkMock;
+    protected readonly TeacherService Service;
+    protected readonly CancellationToken CancellationToken;
+
+    protected TeacherServiceTestBase()
+    {
+        TeacherRepositoryMock = new Mock<ITeacherRepository>();
+        UnitOfWorkMock = new Mock<IUnitOfWork>();
+
+        Service = new TeacherService(
+            TeacherRepositoryMock.Object,
+            UnitOfWorkMock.Object
+        );
+
+        CancellationToken = CancellationToken.None;
+    }
+}
